e290724378826cf416f6664b25295c3ef7b51013
MedinaPeraltaJoaquin <joaquin.medina513@ciencias.unam.mx>
2025-09-14 15:56:24 -0600

    fix: modificacion de los modelos y de sus controladores

--- src/entity/city_with_distance.rs
+++ src/entity/city_with_distance.rs
@@ -1,6 +1,5 @@
-use serde::{Serialize, Deserialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug,Clone)]
 pub struct CityWithDistance {
     pub city: i32,
     pub origin: i32,
@@ -8,9 +7,6 @@ pub struct CityWithDistance {
 }
 
 impl CityWithDistance {
-    pub fn new(city: i32, origin: i32, distance_m: f64) -> Self {
-        CityWithDistance { city, origin, distance_m }
-    }
 
     pub fn get_city(&self) -> i32 {
         self.city
@@ -18,21 +14,5 @@ impl CityWithDistance {
 
     pub fn get_origin(&self) -> i32 {
         self.origin
-    }
-
-    pub fn get_distance_m(&self) -> f64 {
-        self.distance_m
-    }
-
-    pub fn set_city(&mut self, city: i32) {
-        self.city = city;
-    }
-
-    pub fn set_origin(&mut self, origin: i32) {
-        self.origin = origin;
-    }
-
-    pub fn set_distance_m(&mut self, distance_m: f64) {
-        self.distance_m = distance_m;
     }
 }
